e1a1be2a7f765e43cce0678df4e684ddf2d8e8e6
Peter-Zheng1 <343277125@tdsb.ca>
2026-03-30 14:51:50 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,37 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+public static String evenOrOdd(int num){
+    if (num % 2 == 0) {
+        return("Even");
+    }
+        return("Odd");
+
 
 }
+
+public static String teacherOrStudent(String user) {
+    
+    if (user.equals("Kalisz")) {
+        return("Teacher");
+    }
+        return("Student");
+    }
+    
+
+
+    public static int fartherFromZero(int num) {
+        if (num > 0) {
+            return(num + 5);
+        }
+        if (num < 0){
+           return(num - 5);
+            
+        }
+        
+            return(num);
+        }
+    }
+
+
+
